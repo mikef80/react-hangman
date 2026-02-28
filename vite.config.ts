9618209ae4c82a82e5675fa7011f69e8132bc06e
Mike <mike@mike-francis.org>
2026-02-28 18:42:03 +0000

    pwa working on mac

--- vite.config.ts
+++ vite.config.ts
@@ -7,26 +7,30 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
-        name: 'Derailed',
-        short_name: 'Derailed',
-        description: 'A train-based hangman game',
-        theme_color: '#ffffff',
+        name: "Derailed",
+        short_name: "Derailed",
+        description: "A train-based hangman game",
+        display: "standalone",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'web-app-manifest-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'web-app-manifest-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+            src: "web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
       registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
       devOptions: {
         enabled: true,
       },
